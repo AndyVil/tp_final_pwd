3ab8e2986b1a4926da416e9245e6bd5fcaeeff59
AndyVil <70775683+AndyVil@users.noreply.github.com>
2021-11-12 21:22:22 -0300

    Estilo página de inicio

--- view/structure/footer.php
+++ view/structure/footer.php
@@ -1,12 +1,17 @@
-<!--FOOTER======================================================================-->
-<div style="height: 7.5%; width: 99.8%; border: 2px solid red; border-radius: 5px;">
-    <h1>Este es el Pie</h1>
-</div>
+<!--FOOTER======================================================================
+ style="height: 7.5%; width: 99.8%; border: 2px solid red; border-radius: 5px;"-->
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted">Santiago Avilez Ariza | Lucas Villarruel</span>
+    </div>
+</footer>
+
 
 <script src="./../tools/jquery-3.6.0.min.js"></script>
 <script src="./../tools/popper.min.js"></script>
 <script src="./../tools/bootstrap-5.1.3/js/bootstrap.min.js"></script>
 </body>
+
 <!--FINAL HTML==================================================================-->
 
 </html>
--- view/structure/header.php
+++ view/structure/header.php
@@ -3,39 +3,49 @@
 <html lang="en">
 
 <!--HEAD-->
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="./../tools/bootstrap-5.1.3/css/bootstrap.min.css">
-      <title></title>
-  </head>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./../tools/bootstrap-5.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./../css/style.css">
+  <title></title>
+</head>
 
 <!--BODY==================================================================================-->
-  <body>
-  
-      <!--Es el inicio de body y la cabecera-->
-      <?php
-      //LLAMADO A CONFIG==================================================================
-       require_once("../../config.php");
-      ?>
 
-      
-      <!--CABECERA DIV--> 
-       <div style="height: 9%; width: 99.8%; border: 2px solid red; border-radius: 5px;">
-           <h1>Este es la cabecera</h1>
-       </div>
+<body>
+
+  <!--Es el inicio de body y la cabecera-->
+  <?php
+  //LLAMADO A CONFIG==================================================================
+  require_once("../../config.php");
+  ?>
 
 
-      <?php
-      //LLAMADO A LATERAL(SIDE)===========================================================
-       require_once("side.php");
-      ?>
+  <!--CABECERA DIV
+    style="height: 9%; width: 99.8%; border: 2px solid red; border-radius: 5px;"-->
+  <header>
+    <h3 id="titulo">Tienda de ropa</h3>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  href="#">Link</a>
+      </li>
+    </ul>
+</header>
 
-    <!-- 
-    Podes notar aca que en el modelo MVC la cabecera habre un html y el body, luego el lateral
-    simplemente es un codigo de html sin etiquetas de abertura, al cual invocamos debajo de la cabecera
-    y por ultimo el pie es el cierre de las etiquetas html y body.
-    Es importante remarcar que el pie de invoca al finald el cuerpo de la pagina (index.php), ya que si lo
-    llamamos al final de la cabecera, este se sobrepondria con el cuerpo
-    -->
+
+  <?php
+  //LLAMADO A LATERAL(SIDE)===========================================================
+  //require_once("side.php");
+  ?>
