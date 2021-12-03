2bc2967e30311f96bcc5b09f0210d9efa1781f00
AndyVil <70775683+AndyVil@users.noreply.github.com>
2021-12-03 13:28:07 -0300

    Alert para json

--- view/inicio_deposito/index.php
+++ view/inicio_deposito/index.php
@@ -22,10 +22,10 @@ $listaTabla = $objAbmTabla->buscar(null);
 </div>
 
 <div class="row mb-5">
-
-    <form id="productosCargados" name="productosCargados" action="actualizarProducto.php" method="POST">
+    <!-- action="actualizarProducto.php" -->
+    <form id="productosCargados" name="productosCargados" method="POST">
         <div class="table-responsive">
-            <table class="table table-striped">
+            <table class="table table-striped" id="listarProductos">
                 <thead>
                     <br>
                     <h3 align="center">Listar Productos</h3>
@@ -33,7 +33,7 @@ $listaTabla = $objAbmTabla->buscar(null);
                     <?php
                     if (array_key_exists('mensaje', $url)) {
                         echo "  <div class='alert alert-success mt-3' role='alert' align=center>
-                        ".$url['mensaje']."
+                        " . $url['mensaje'] . "
                         </div>";
                     }
                     ?>
@@ -74,7 +74,7 @@ $listaTabla = $objAbmTabla->buscar(null);
                         //SVG son los iconos de los botones
                         echo '
                         <td class="text-center">
-                            <button class="btn btn-success btn-sm" type="submit" value="' . $id . '" id="proEdit" name="proEdit" role="button">
+                            <button class="btn btn-success btn-sm" type="submit" value="' . $id . '" formaction="actualizarProducto.php" id="proEdit" name="proEdit" role="button">
                                 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -83,7 +83,7 @@ $listaTabla = $objAbmTabla->buscar(null);
 
                             </button>
 
-                            <button class="btn btn-danger btn-sm" type="submit" value="' . $id . '" formaction="eliminarProducto.php" id="proBorrar" name="proBorrar" role="button">
+                            <button class="btn btn-danger btn-sm" type="submit" value="' . $id . '" onclick="desactivarProducto()" id="proBorrar" name="proBorrar" role="button">
                                 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
@@ -93,6 +93,7 @@ $listaTabla = $objAbmTabla->buscar(null);
                         </td>';
                         echo '</tr>';
                         $i++;
+                        //formaction="eliminarProducto.php" DONDE ESTA EL ONCLICK
                     }
 
                     echo '</tbody>';
@@ -109,6 +110,22 @@ $listaTabla = $objAbmTabla->buscar(null);
         </div>
     </form>
 </div>
+
+
+
+<script type="text/javascript">
+    function desactivarProducto() {
+
+        let confirmar = confirm("¿Desea deshabilitar este producto?");
+        if (confirmar) {
+            //Aquí pones lo que quieras si da a Aceptar
+            alert('Cambio aceptado.');
+        } else {
+            //Aquí pones lo que quieras Cancelar
+            alert('Cambio cancelado.');
+        }
+    }
+</script>
 
 <?php
 //FOOTER============================================================================
